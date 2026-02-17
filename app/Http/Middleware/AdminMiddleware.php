7e3fe2f2b6b6440acd3af7868f298d09f009cbbc
Tajim <com.tajim@gmail.com>
2026-02-17 21:00:26 +0600

    required 2FA on for admin accounts

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -15,9 +15,15 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next): Response
     {
-         if (!$request->user() || $request->user()->role !== 'admin' || !$request->user()->hasVerifiedEmail()) {
+         if (!$request->user() || $request->user()->role !== 'admin') {
         abort(403);
+            } else if ( !$request->user()->hasVerifiedEmail()){
+                abort(403, 'Email not verified.');
             }
+            else if ($request->user()->two_factor_secret === null) {
+                abort(403, 'Two-factor authentication not enabled.');
+            }   
+
         return $next($request);
     }
 }
