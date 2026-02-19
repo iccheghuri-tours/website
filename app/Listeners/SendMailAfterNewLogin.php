b9b4c498de3e9653ec3c7629fd662620fa0b9650
Tajim <com.tajim@gmail.com>
2026-02-19 15:31:05 +0600

    notified user via mail after new login

---
+++ app/Listeners/SendMailAfterNewLogin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Mail\SimpleMail;
+use Illuminate\Auth\Events\Login;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Mail;
+
+class SendMailAfterNewLogin
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(Login $event): void
+    {
+        //
+        $user = $event->user;
+        $ipAddress = request()->ip();
+        
+        Mail::to($user->email)->send(new SimpleMail([
+            'subject' => 'New Login Detected',
+            'message' => 'A new login to your account was detected from IP address: ' . $ipAddress . '. If this was not you, please secure your account immediately.',
+        ]));
+    }
+}
---
+++ resources/views/emails/simple.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <meta charset="utf-8">
+    <title>{{ $data['subject'] }}</title>
+    <body>
+        <h1>{{ $data['subject'] }}</h1>
+        <p>{{ $data['message'] }}</p>
+    </body>
+</html>
