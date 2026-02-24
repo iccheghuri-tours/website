37656701b9c70e3469141ed84281b09513b1c6c6
Tajim <com.tajim@gmail.com>
2026-02-24 23:18:55 +0600

    implemented shouldQueue for mails

--- app/Mail/SimpleMail.php
+++ app/Mail/SimpleMail.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class SimpleMail extends Mailable
+class SimpleMail extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
